9b29054cddf255d8968db9fb7b1850ef2cf7845c
Shammy Alam <s.alam@donboscosandona.it>
2025-11-07 13:09:54 +0100

    .

---
+++ prova1.js
@@ -0,0 +1,11 @@
+let cateto1 = 5;
+let cateto2 = 7;
+
+let ipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2,2));
+
+console.log(ipotenusa);
+
+
+let risultatoStinga = ipotenusa.toFixed(2)
+
+console.log(risultatoStinga);
---
+++ prova2.js
@@ -0,0 +1,16 @@
+let frase;
+
+let semaforo = "verde"
+
+if (semaforo == "verde") {
+    frase = "Il semaforo è verde"
+} else if (semaforo == "rosso") { 
+    frase = "Il semaforo è rosso"
+} else if (semaforo == "arancione") { 
+    frase = "Il semaforo è arancione"
+} else if (semaforo == "arancio") { 
+    frase = "Il semaforo è arancio"
+} else if (semaforo == "giallo") { 
+    frase = "Il semaforo è giallo"
+}
+console.log(frase)
